26ef40c7b839b0884bdc316e647e40f3569bfa2f
EdgarC <ey.camacho10@gmail.com>
2019-11-18 15:23:27 -0500

    Change in Lazy Test

--- src/test/java/com/globant/spring/c/lazyinit/LazyInitTest.java
+++ src/test/java/com/globant/spring/c/lazyinit/LazyInitTest.java
@@ -9,22 +9,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class LazyInitTest {
 
     private LazyBean lazyBean;
-    private NoLazyBean noLazyBean;
     private ApplicationContext ctx;
 
     @Before
     public void init() {
         System.setProperty("spring.profiles.active", "test");
         this.ctx = new ClassPathXmlApplicationContext("spring-test-config.xml");
-        noLazyBean = (NoLazyBean) ctx.getBean("noLazyBean");
     }
 
     @Test
     public void testLazyInitBeanProcess() {
-        String res = noLazyBean.process();
-        Assert.assertEquals("message from NoLazyBean -> process", res);
         lazyBean = (LazyBean) ctx.getBean("lazyBean");
-        res = lazyBean.process();
+        String res = lazyBean.process();
         Assert.assertEquals("message from LazyBean -> process", res);
     }
 }
